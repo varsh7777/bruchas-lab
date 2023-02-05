7a352bf1e027994b3a31b7ebcb93855afd2daf8d
Sri Varshitha Pinnaka <102142453+varsh7777@users.noreply.github.com>
2023-02-05 09:53:20 -0800

    Add files via upload

---
+++ state behavior data classification/pain assay rf classifier.py
@@ -0,0 +1,43 @@
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+# import numpy as np
+
+# reading csv of state behavior in for analysis
+data = pd.read_csv("C:/Users/sriva/Downloads/pain assay.csv")
+
+# separating feature values from labels, displaying them
+x = data.iloc[:, [2, 3, 4]].values
+print(x)
+y = data.iloc[:, 1].values
+print(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+
+classifier = RandomForestClassifier(n_estimators=200)
+
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+
+# accuracy of classifier
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+# importance of each feature
+feature_imp = pd.Series(classifier.feature_importances_,
+                        index=['Time to flick',
+                               'Von frey',
+                               'Latency to lick']).sort_values(ascending=False)
+
+print(feature_imp)
+
+"""
+Accuracy: 0.5294117647058824
+
+Feature importance:
+Von frey           0.373945
+Time to flick      0.316860
+Latency to lick    0.309195
+"""
