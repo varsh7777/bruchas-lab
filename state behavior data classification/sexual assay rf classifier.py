5fb88588e2fc19dc9052470970c4131fdfcb22ba
Sri Varshitha Pinnaka <102142453+varsh7777@users.noreply.github.com>
2023-02-05 09:59:08 -0800

    Update sexual assay rf classifier.py

--- state behavior data classification/sexual assay rf classifier.py
+++ state behavior data classification/sexual assay rf classifier.py
@@ -6,7 +6,7 @@ from sklearn import metrics
 # import numpy as np
 
 # reading csv of state behavior in for analysis
-data = pd.read_csv("C:/Users/sriva/Downloads/sexual assay.csv")
+data = pd.read_csv("sexual assay.csv")
 
 # separating feature values from labels, displaying them
 x = data.iloc[:, [2]].values
