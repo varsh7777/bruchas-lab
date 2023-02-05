5eaf39000e220cacce8c10eb554c40cd4efc1b88
Sri Varshitha Pinnaka <102142453+varsh7777@users.noreply.github.com>
2023-02-05 08:33:28 -0800

    Create running wheel feature classifier.py

---
+++ state behavior data classification/running wheel feature classifier.py
@@ -0,0 +1,32 @@
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+# import numpy as np
+
+# reading csv of state behavior in for analysis
+data = pd.read_csv("C:/Users/sriva/Downloads/state behavior data 1.csv")
+
+# separating x values from labels
+x = data.iloc[:, [2, 3, 4]].values
+print(x)
+y = data.iloc[:, 1].values
+print(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5,
+                                                    random_state=0)
+
+classifier = RandomForestClassifier(n_estimators=200)
+
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+feature_imp = pd.Series(classifier.feature_importances_,
+                        index=['Time on wheel', 'latency to run',
+                               'Frequency']).sort_values(ascending=False)
+
+print(feature_imp)
