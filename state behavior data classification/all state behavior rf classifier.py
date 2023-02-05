7b220db21516ab9ddd76e704f4d30947e5df827e
Sri Varshitha Pinnaka <102142453+varsh7777@users.noreply.github.com>
2023-02-05 10:56:44 -0800

    Update all state behavior rf classifier.py

--- state behavior data classification/all state behavior rf classifier.py
+++ state behavior data classification/all state behavior rf classifier.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
 # from sklearn.preprocessing import StandardScaler
+# accuracy tended to go up when data was scaled, but not too much?
 from sklearn import metrics
 # import numpy as np
 
